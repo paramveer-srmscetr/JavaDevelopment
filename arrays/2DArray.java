3d9073bf47306738bf726ec48fe1192667756f20
Paramveer singh <82169480+paramveer-srmscetr@users.noreply.github.com>
2026-01-13 18:21:43 +0530

    Create 2DArray.java

---
+++ arrays/2DArray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+   
+     Scanner sc= new Scanner(System.in);	
+   List<List<Integer>>ls= new ArrayList<>();
+   
+   int row=sc.nextInt();
+   int col= sc.nextInt();
+   
+   for(int i=0;i<row;i++){
+          ls.add(new ArrayList<>());
+       for(int j=0;j<col;j++){
+           int n=sc.nextInt();
+          ls.get(i).add(n);
+           
+       }
+   }
+   
+   
+   System.out.println(ls);
+   
+	}
+}
